e41fecef969f30c391fcefd6c7366fc39b869bd3
Matt Neal <impulseimg@gmail.com>
2020-08-25 19:08:14 -0400

    Schema node

---
+++ schema.sql
@@ -0,0 +1,7 @@
+-- Drops the labor_db if it exists currently --
+DROP DATABASE IF EXISTS labor_db;
+-- Creates the "labor_db" database --
+CREATE DATABASE labor_db;
+
+-- UserId 1 = bob@bobmail.com  password=password
+-- UserId 2 = aaron@gmail.com  password=password
